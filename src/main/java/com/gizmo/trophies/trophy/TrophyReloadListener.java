ff44ce5824d39eb01531759e850f01f06eb175f5
GizmoTheMoonPig <67468252+GizmoTheMoonPig@users.noreply.github.com>
2022-10-21 21:15:11 -0600

    only load trophies if the mod is loaded

--- src/main/java/com/gizmo/trophies/trophy/TrophyReloadListener.java
+++ src/main/java/com/gizmo/trophies/trophy/TrophyReloadListener.java
@@ -10,6 +10,7 @@ import net.minecraft.server.packs.resources.ResourceManager;
 import net.minecraft.server.packs.resources.SimpleJsonResourceReloadListener;
 import net.minecraft.util.GsonHelper;
 import net.minecraft.util.profiling.ProfilerFiller;
+import net.minecraftforge.fml.ModList;
 import net.minecraftforge.registries.ForgeRegistries;
 
 import java.util.HashMap;
@@ -28,15 +29,18 @@ public class TrophyReloadListener extends SimpleJsonResourceReloadListener {
 	protected void apply(Map<ResourceLocation, JsonElement> map, ResourceManager manager, ProfilerFiller profiler) {
 		validTrophies.clear();
 		map.forEach((resourceLocation, jsonElement) -> {
-			try {
-				JsonObject object = GsonHelper.convertToJsonObject(jsonElement, "trophy");
-				Trophy trophy = Trophy.fromJson(object);
-				//only add entries if the entity exists, otherwise skip it
-				if (ForgeRegistries.ENTITY_TYPES.containsValue(trophy.type())) {
-					validTrophies.put(ForgeRegistries.ENTITY_TYPES.getKey(trophy.type()), trophy);
+			//check if the mod is loaded first. Since we read trophies before knowing the entity, any behaviors that are modded or use modded items will spit out an error.
+			if (ModList.get().isLoaded(resourceLocation.getNamespace())) {
+				try {
+					JsonObject object = GsonHelper.convertToJsonObject(jsonElement, "trophy");
+					Trophy trophy = Trophy.fromJson(object);
+					//only add entries if the entity exists, otherwise skip it
+					if (ForgeRegistries.ENTITY_TYPES.containsValue(trophy.type())) {
+						validTrophies.put(ForgeRegistries.ENTITY_TYPES.getKey(trophy.type()), trophy);
+					}
+				} catch (Exception exception) {
+					OpenBlocksTrophies.LOGGER.error("Caught an error loading trophy config for {}! {}", resourceLocation, exception.getMessage());
 				}
-			} catch (Exception exception) {
-				OpenBlocksTrophies.LOGGER.error("Caught an error loading trophy config for {}! {}", resourceLocation, exception.getMessage());
 			}
 		});
 		OpenBlocksTrophies.LOGGER.info("Loaded {} Trophy configs.", validTrophies.size());
