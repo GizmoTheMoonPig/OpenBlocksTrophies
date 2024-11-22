19ca561c65f1f180104adb319bf9aa3446c1f9bd
GizmoTheMoonPig <67468252+GizmoTheMoonPig@users.noreply.github.com>
2024-11-22 14:59:11 -0700

    cache click sound events

--- src/main/java/com/gizmo/trophies/trophy/AmbientSoundFetcher.java
+++ src/main/java/com/gizmo/trophies/trophy/AmbientSoundFetcher.java
@@ -12,11 +12,14 @@ import net.minecraftforge.fml.util.ObfuscationReflectionHelper;
 import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
 import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
 
 public class AmbientSoundFetcher {
 	private static final MethodHandles.Lookup LOOKUP = MethodHandles.lookup();
 	private static final Method Mob_getAmbientSound = ObfuscationReflectionHelper.findMethod(Mob.class, "m_7515_");
 	private static final MethodHandle handle_Mob_getAmbientSound;
+	private static final Map<EntityType<?>, Pair<SoundEvent, Float>> SOUND_CACHE = new HashMap<>();
 
 	static {
 		MethodHandle tmp_handle_Mob_getAmbientSound = null;
@@ -30,19 +33,24 @@ public class AmbientSoundFetcher {
 
 	//fetches both the ambient sound and voice pitch to avoid creating multiple entity instances per sound call
 	public static Pair<SoundEvent, Float> getAmbientSoundAndPitch(EntityType<?> type, Level level) {
-		SoundEvent sound = null;
-		float pitch = 1.0F;
-		Entity entity = type.create(level);
-		if (handle_Mob_getAmbientSound != null && entity instanceof Mob mob) {
-			try {
-				sound = (SoundEvent) handle_Mob_getAmbientSound.invokeExact(mob);
-			} catch (Throwable e) {
-				//fail silently, doesn't matter as this method can be null
+		if (!SOUND_CACHE.containsKey(type)) {
+			SoundEvent sound = null;
+			float pitch = 1.0F;
+			Entity entity = type.create(level);
+			if (handle_Mob_getAmbientSound != null && entity instanceof Mob mob) {
+				try {
+					sound = (SoundEvent) handle_Mob_getAmbientSound.invokeExact(mob);
+				} catch (Throwable e) {
+					//fail silently, doesn't matter as this method can be null
+				}
 			}
+			if (entity instanceof LivingEntity living) {
+				pitch = living.getVoicePitch();
+			}
+			var pair = Pair.of(sound, pitch);
+			SOUND_CACHE.put(type, pair);
+			return pair;
 		}
-		if (entity instanceof LivingEntity living) {
-			pitch = living.getVoicePitch();
-		}
-		return Pair.of(sound, pitch);
+		return SOUND_CACHE.get(type);
 	}
 }
